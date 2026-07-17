131b6b1e17a85c8a8a0a2f3febf95c7a96503d2d
pismichos <johnny_pismixos@hotmail.com>
2026-07-17 18:32:05 +0300

    Add Course model and update database schema

--- courses/models.py
+++ courses/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 class Course(models.Model):
-    title = models.CharField(max_length=100)
+    
+    LEVEL_CHOICES = [
+        ('beginner', 'Beginner'),
+        ('intermediate', 'Intermediate'),
+        ('advanced', 'Advanced'),]
+    
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     description = models.TextField()
+    level = models.CharField(max_length=20, choices=LEVEL_CHOICES, default='beginner')
+    published = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Course
 
-# Create your views here.
+
+def course_list(request):
+    courses = Course.objects.filter()
+
+    context = {
+        "courses": courses
+    }
+
+    return render(
+        request,
+        "courses/course_list.html",
+        context
+    )
