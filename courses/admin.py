3b095668409db1213c291f65e25a3517518c0447
pismichos <johnny_pismixos@hotmail.com>
2026-07-28 12:07:17 +0300

    Configure Course admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Course
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "title",
+        "level",
+        "published",
+        "created_at",
+    )
+
+    list_filter = (
+        "level",
+        "published",
+    )
+
+    search_fields = (
+        "title",
+        "description",
+    )
+
+    prepopulated_fields = {
+        "slug": ("title",)
+    }
